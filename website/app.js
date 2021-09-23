6befdfabb5152ed3dc32c50132ef5bd6cb23d837
ryan hughes <ryanhuga@gmail.com>
2021-09-23 08:43:02 -0700

    Update app.js

--- website/app.js
+++ website/app.js
@@ -54,7 +54,8 @@ app.use(express.static("public"));
   GET ENDPOINTS
 */
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
+  console.log(path.join(__dirname, "public/index"));
 });
 
 app.get("/boost", function (req, res) {
